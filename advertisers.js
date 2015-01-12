b95827085cd5994299349d33071c2b1f92a0a7a9
Paul Edwards <paul.edwards@cheapflights.com>
2015-01-12 11:59:49 +0000

    Added new api call

--- advertisers.js
+++ advertisers.js
@@ -23,7 +23,7 @@ router
 				return collection.length;
 			}, res
 		);
-	})	
+	})
 	.get('/:search', function(req, res) {
 		getAdvertisers(
 			function(collection) {
@@ -39,15 +39,41 @@ router
 				});
 
 				return filteredAdvertisers.map(function(advertiser) {
-					return { 
-						id: advertiser._id, 
-						name: advertiser.Name				
+					return {
+						id: advertiser._id,
+						name: advertiser.Name
 					};
 				}).sort(function (a, b) {
 					return a.name.localeCompare(b.name);
 				})
 			}, res
 		);
+	})
+	.get('/filter/:n', function(req, res) {
+		getAdvertisers(function(collection) {
+
+			return collection.filter(function(element, index) {
+				return index % req.params.n === 0 && element.Brands.length > 0;
+			})
+			.map(function(advertiser) {
+				return advertiser.Brands.map(function(brand) {
+					return {
+						advertiser: advertiser.Name,
+						brand: brand.Name
+					}
+				});
+			})
+			.reduce(function(a, b) {
+				return a.concat(b);
+			})
+			.sort(function(a, b) {
+				if (a.advertiser !== b.advertiser)
+					return a.advertiser.localeCompare(b.advertiser);
+
+				return a.brand.localeCompare(b.brand);
+			});
+
+		}, res);
 	});
 
 // Return router
